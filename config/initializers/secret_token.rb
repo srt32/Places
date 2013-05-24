9fc72495233b9e3f133104f4cf459c2ba9ea530c
Simon <simon.taranto@gmail.com>
2013-05-23 18:29:15 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Places::Application.config.secret_token = 'a8ff6b56bd837a7966dd889c5125d41c5d659f815b08d36318b63390bba47a326fdbbb1acbf945f746ecbdb1059da40b9469bbf8e44835698d7dda40548997d9'
