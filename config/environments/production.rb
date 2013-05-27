f2b56ec97ede69ff6d0405dcb2d18a82df534085
Simon <simon.taranto@gmail.com>
2013-05-26 20:03:27 -0600

    set config.serve_static_assets = true to try and fix blank prod maps

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,9 @@ Places::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
+  # updated from http://stackoverflow.com/questions/7155053/gmaps4rails-not-showing-in-production
+  # because gmaps were blank on prod
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
