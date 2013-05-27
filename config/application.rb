b9682b512de77e73bb5d128bbb72b46eb905dfa0
Simon <simon.taranto@gmail.com>
2013-05-26 21:05:14 -0600

    add config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/ to application.rb from TMS app

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,9 @@ module Places
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # copied from TMS app
+    config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
