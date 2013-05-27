739a086775e8b5234875f1acd100d3f72938011e
Simon <simon.taranto@gmail.com>
2013-05-26 20:08:41 -0600

    double quotes on gmaps

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,8 @@ Places::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
+  # set back to false
   # updated from http://stackoverflow.com/questions/7155053/gmaps4rails-not-showing-in-production
   # because gmaps were blank on prod
 
