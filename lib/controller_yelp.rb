3146e893e1ca83ad6aad570d5458cd04c54bb107
Simon <simon.taranto@gmail.com>
2013-05-28 22:57:09 -0600

    first swipe at directions for stops

--- lib/controller_yelp.rb
+++ lib/controller_yelp.rb
@@ -1,6 +1,6 @@
 module ControllerYelp
 	def self.included(controller)
-		controller.send :helper_method, :get_yelp_search
+		controller.send :helper_method, :get_yelp_search, :get_gmaps_directions
 	end
 
 	def get_yelp_search(searchType,school)
@@ -8,4 +8,9 @@ module ControllerYelp
     @name = @name_raw.gsub(/[ ]/, '+')
     link = "http://www.yelp.com/search?find_desc=" + searchType + "&find_loc=" + @name
   end
+
+  def get_gmaps_directions(school0,school1)
+  	school0.to_s + school1.to_s
+  end
+
 end
