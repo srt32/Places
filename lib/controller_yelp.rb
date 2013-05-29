0ef529a876872a711b6ac82ae10ddc5c7f8a275a
Simon <simon.taranto@gmail.com>
2013-05-28 22:22:15 -0600

    add hotels yelp search closes #22

--- lib/controller_yelp.rb
+++ lib/controller_yelp.rb
@@ -1,9 +1,9 @@
 module ControllerYelp
 	def self.included(controller)
-		controller.send :helper_method, :get_yelp_restaurants
+		controller.send :helper_method, :get_yelp_search
 	end
 
-	def get_yelp_restaurants(searchType,school)
+	def get_yelp_search(searchType,school)
     @name_raw = school.name
     @name = @name_raw.gsub(/[ ]/, '+')
     link = "http://www.yelp.com/search?find_desc=" + searchType + "&find_loc=" + @name
