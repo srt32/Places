96dd0a9f8383b5815fe8b36b7fb0d8ad1a5b201c
Simon <simon.taranto@gmail.com>
2013-06-04 19:11:03 -0600

    update open/closed text

--- lib/campus_calendar.rb
+++ lib/campus_calendar.rb
@@ -12,7 +12,7 @@ module CampusCalendar
 		travelDays = travelDates(trip)
 		closedDays = schoolDates(stop)
 		overlap = travelDays & closedDays
-		overlap.count > 0 ? "CLOSED" + overlap.to_s : "OPEN"
+		overlap.count > 0 ? false : true
 	end
 
 	def schoolDates(stop)
