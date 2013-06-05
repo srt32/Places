afeaf6d4c1d103b6e5bfb63f9217855fad4aff78
Simon <simon.taranto@gmail.com>
2013-06-04 19:30:16 -0600

    update trip redirect to go to index closes#28

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -56,7 +56,7 @@ class TripsController < ApplicationController
 
     respond_to do |format|
       if @trip.save
-        format.html { redirect_to root_path, notice: 'Trip was successfully created.' }
+        format.html { redirect_to user_trips_path, notice: 'Trip was successfully created.' }
         format.json { render json: @trip, status: :created, location: @trip }
       else
         format.html { render action: "new" }
