cadeb27311c51351405815a933364321ad4f65e6
Simon <simon.taranto@gmail.com>
2013-06-08 16:28:08 -0600

    gmaps work cleanup, no resolution exit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,13 +48,16 @@ Places::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w[gmaps4rails.css]
+  # config.assets.precompile += %w[gmaps4rails.css]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
   # config.threadsafe!
+
+  # from http://stackoverflow.com/questions/10792598/heroku-ruby-on-rails-error-image-not-precompiling
+  config.assets.initialize_on_precompile = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
