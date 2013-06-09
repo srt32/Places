2e4715fc8d42ce28044732859ed8893acf5da730
Simon <simon.taranto@gmail.com>
2013-06-09 11:40:23 -0600

    redirect after user sign in, closes ##85

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => "Whoops, that's a no no."
   end
+
+  def after_sign_in_path_for(resource)
+ 		user_trips_path(current_user)
+	end
 end
